1ae72811cf0ebd5e0537250138f3fe2d3570e449
shalala0115 <shalala0115@gmail.com>
2020-12-01 21:04:46 -0800

    Updates

---
+++ precache-manifest.c381927a68c2d40d78e6e01828ce09f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d415fe2cb122b42c8296e5eb596c4eea",
+    "url": "/github.io/lendcar/index.html"
+  },
+  {
+    "revision": "941c6842c1ddbc86afac",
+    "url": "/github.io/lendcar/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "6f70d592f41a2fc8c59c",
+    "url": "/github.io/lendcar/static/css/main.a6e59609.chunk.css"
+  },
+  {
+    "revision": "941c6842c1ddbc86afac",
+    "url": "/github.io/lendcar/static/js/2.2c382003.chunk.js"
+  },
+  {
+    "revision": "e32df3232e2510e752045a021f8faea5",
+    "url": "/github.io/lendcar/static/js/2.2c382003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f70d592f41a2fc8c59c",
+    "url": "/github.io/lendcar/static/js/main.a79e5a78.chunk.js"
+  },
+  {
+    "revision": "997c04f7b9ba8483ac50",
+    "url": "/github.io/lendcar/static/js/runtime-main.2f1a2042.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/lendcar/static/media/logo.5d5d9eef.svg"
+  }
+]);
